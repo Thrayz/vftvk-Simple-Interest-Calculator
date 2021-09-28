7873315dea404a2c4c33091145d929d33cb408c6
Thrayz <88392163+Thrayz@users.noreply.github.com>
2021-09-28 21:24:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,6 @@ function compute()
 //Updating the rate value when the slider is changed
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
 
